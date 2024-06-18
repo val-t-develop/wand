ed95b062beec42cfc0c8e0c169ab472d2f4d52e8
val.t.develop <val.t.develop@gmail.com>
2024-06-18 19:53:10 +0300

    Add testing system. Add test #1.

---
+++ tests/1/test.sh
@@ -0,0 +1,29 @@
+#  SPL - Simple Programming Language compiler
+#  Copyright (C) 2022  Valentyn Tymchyshyn
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or any
+#  later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#  Valentyn Tymchyshyn (val.t.develop) (val.t.develo@gmail.com)
+#
+#  Bash script for automated testing.
+
+#!/bin/bash
+rm -rd log spl test.exe test.result exe &> /dev/null
+mkdir log
+cp -r ./../../res/spl ./spl
+./../../builddir/spl . -o ./exe &> log/build.log
+c++ -std=c++20 test.cpp -o test.exe > log/test.build.log
+./exe>log/exe.log
+echo $?>log/exe.code.log
+./test.exe
